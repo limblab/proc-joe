a6f87f8628d462e17c561e5ce3362988b8f74a80
josephsombeck <jtsombeck@gmail.com>
2017-07-07 12:06:39 -0500

    initial pressure sensor code

---
+++ PressureSensors/NPA201/NPA201_run/firmware/NPA201_run.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=NPA201_run.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/NPA201_run.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=npa201run.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/npa201run.x.tar
