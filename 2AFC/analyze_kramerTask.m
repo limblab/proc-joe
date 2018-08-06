33bb8c15d5d27f4bd2592621e3441e74c6d25e9a
Joseph Tebben Sombeck <jts3256@fsm.northwestern.edu>
2018-08-06 15:04:00 -0500

    code to analyze kramer task

--- 2AFC/analyze_kramerTask.m
+++ 2AFC/analyze_kramerTask.m
@@ -1,161 +1,87 @@
 %% determine filename and input data
-    inputData.folderpath = 'C:\Users\jts3256\Desktop\Han_20180802_BD\';
-    inputData.mapFileName = 'mapFileR:\limblab\lab_folder\Animal-Miscellany\Han_13B1\map files\Left S1\SN 6251-001459.cmp';
+    input_data.folderpath = 'C:\Users\jts3256\Desktop\Han_20180804-05_90deg270deg\';
+    input_data.mapFileName = 'mapFileR:\limblab\lab_folder\Animal-Miscellany\Han_13B1\map files\Left S1\SN 6251-001459.cmp';
 
-    inputData.task='taskBD';
-    inputData.ranBy='ranByJoseph'; 
-    inputData.array1='arrayLeftS1'; 
-    inputData.monkey='monkeyHan';
-    inputData.labnum = 6;
+    input_data.task='taskBD';
+    input_data.ranBy='ranByJoseph'; 
+    input_data.array1='arrayLeftS1'; 
+    input_data.monkey='monkeyHan';
+    input_data.labnum = 6;
     
     pwd=cd;
-    cd(inputData.folderpath)
+    cd(input_data.folderpath)
     fileList = dir('*.nev*');
     cd(pwd)
 %% load in cds and extract data
     td_all = [];
-    num_trials = 0;
-        
-    cds = commonDataStructure();
-    cds.file2cds([inputData.folderpath fileList(1).name],inputData.task,inputData.ranBy,...
-        inputData.monkey,inputData.labnum,inputData.array1,inputData.mapFileName,'recoverPreSync');
-    cd(pwd);
 
-% convert cds to trial data
-    params.event_list = {'tgtDir';'isPrimaryTgt';...
-        'bumpTime';'bumpDir';'bumpMagnitude';'isStimTrial';'stimCode'};
-    params.trial_results = {'R','F'};
-    params.extra_time = [1,2];
-    params.exclude_units = [0:255];
-    td_all = parseFileByTrial(cds,params);
+    for fileNumber = 2%:numel(fileList)  
+        cds = commonDataStructure();
+        cds.file2cds([input_data.folderpath fileList(fileNumber).name],input_data.task,input_data.ranBy,...
+            input_data.monkey,input_data.labnum,input_data.array1,input_data.mapFileName,'recoverPreSync');
+        cd(pwd);
 
-%% plot psychometric curves
+    % convert cds to trial data
+        params.event_list = {'tgtDir';'isPrimaryTgt';...
+            'bumpTime';'bumpDir';'bumpMagnitude';'isStimTrial';'stimCode'};
+        params.trial_results = {'R','F'};
+        params.extra_time = [1,2];
+        params.exclude_units = [0:255];
+        td_temp = parseFileByTrial(cds,params);
+
+        td_all = [td_all, td_temp];
+    end
+%% get psychometric curve data
 % note: all bumpDir's are relative to tgtDir. Is primary target determines
 % if the tgt in tgtDir or the opposite one is the primary (1 = tgtDir, 0 =
 % other target).
-
+    
 % logic for making these curves: For each bump direction, count the total
 % rewards and trials to get a percent correct. Then, do 1-that percent for
 % any bump dir > 90 as rewards here represent the opposite target
-
-    bump_trial_mask = [td_all.isStimTrial] == 0 & [td_all.bumpMagnitude] > 0;
-    td_bump = td_all(bump_trial_mask);
-    bump_dirs = unique([td_bump.bumpDir]);
-    bump_dirs = bump_dirs(~isnan(bump_dirs));
-    bump_dirs = bump_dirs(bump_dirs <= 180);
-    bump_correct = zeros(size(bump_dirs));
-    bump_total = zeros(size(bump_dirs));
+    input_data.max_trial_time = 50000;
+    input_data.min_trial_time = 0;
     
-    for t = 1:numel(td_bump)
-        if(td_bump(t).bumpDir > 180)
-            bump_idx = find(bump_dirs == -1*(td_bump(t).bumpDir-360));
-        else
-            bump_idx = find(bump_dirs == td_bump(t).bumpDir);
-        end
+    input_data.num_bootstrap = 0;
         
-        if(td_bump(t).result == 'R')
-            bump_correct(bump_idx) = bump_correct(bump_idx) + 1;
-        end
-        bump_total(bump_idx) = bump_total(bump_idx) + 1;
-    end
-
-    bump_percent_correct = bump_correct./bump_total;
+    psych_data = getPsychometricCurveData(td_all,input_data);
     
-    % 1-bump_percent_correct for dirs > 90 to make these figures
-    bump_percent_correct(bump_dirs > 90) = 1 - bump_percent_correct(bump_dirs > 90);
+%% plot psych data
     
-    % make psychometric curve
-    figure();
-    plot(bump_dirs,bump_percent_correct,'k.','markersize',14)
-    xlabel('Bump Direction')
-    ylabel('Percent to 0-deg target')
-    xlim([0,180])
-    ylim([0,1])
-    formatForLee(gcf)
-    set(gca,'fontsize',14)
+    input_data.colors = {'k','r','b',[0,0.5,0]};
+    f = plotPsychometricCurve(psych_data(1:end),input_data);
+
+% legend
+    ax = gca;
+    ax.Children;
+    uistack(ax.Children(3),'top');
+    uistack(ax.Children(5),'top');
+    l=legend('bump','0-deg','180-deg');
+    set(l,'box','off');
     
-% fit psychometric curve
-    s = fitoptions('Method','NonlinearLeastSquares', 'Startpoint', [.5 -0.5 .1 100],...
-        'MaxFunEvals',10000,'MaxIter',1000,'TolFun',10E-8,'TolX',10E-8,...
-        'Weights',bump_total/sum(bump_total));
-    ft = fittype('a+b*(erf(c*(x-d)))','options',s);
-
-    [g_mdl.fitObj,g_mdl.gof] = fit(bump_dirs', bump_percent_correct', ft);
-
-    x_fit = [0:180];
-    y_fit = feval(g_mdl.fitObj,x_fit);
-%       
+%% save
+    f = gcf;
+    f.Name = 'Han_20180805_BD_psychometricCurve';
+    saveFiguresLIB(f,input_data.folderpath,f.Name);
+    
+    
+%% draw circle with line in 0 deg direction
+    f = figure;
+    theta = [45,45;225,225];
+    
+    rho = [0,1];
+    polarplot(theta(1,:)*pi/180,rho,'color','r','linewidth',3);
     hold on
-    plot(x_fit,y_fit,'color','k','linewidth',1.5)
-% plot for stim trials
-    stim_codes = unique([td_all.stimCode]);
-    stim_codes = stim_codes(~isnan(stim_codes));
-    colors = {'r','b'};
-    
-    for st = 1:numel(stim_codes)
-        stim_trial_mask = [td_all.isStimTrial] == 1 & [td_all.stimCode] == stim_codes(st) & [td_all.bumpMagnitude] > 0;
-        td_stim = td_all(stim_trial_mask);
-        bump_dirs = unique([td_stim.bumpDir]);
-        bump_dirs = bump_dirs(~isnan(bump_dirs));
-        bump_dirs = bump_dirs(bump_dirs <= 180);
-        bump_correct = zeros(size(bump_dirs));
-        bump_total = zeros(size(bump_dirs));
+    polarplot(theta(2,:)*pi/180,rho,'color','b','linewidth',3);
+    ax = f.Children;
+    ax.RTickLabel = {};
+    ax.ThetaTick = [theta(1,1),theta(2,1)];
+    ax.ThetaTickLabel = {'0','180'};
+    ax.ThetaMinorGrid = 'on';
+    ax.LineWidth = 2;
+    ax.LineColor = 'k';
 
-        for t = 1:numel(td_stim)
-            if(td_stim(t).bumpDir > 180)
-                bump_idx = find(bump_dirs == -1*(td_stim(t).bumpDir-360));
-            else
-                bump_idx = find(bump_dirs == td_stim(t).bumpDir);
-            end
-            if(td_stim(t).result == 'R')
-                bump_correct(bump_idx) = bump_correct(bump_idx) + 1;
-            end
-            bump_total(bump_idx) = bump_total(bump_idx) + 1;
-        end
-
-        bump_percent_correct = bump_correct./bump_total;
-
-        % 1-bump_percent_correct for dirs > 90 to make these figures
-        bump_percent_correct(bump_dirs > 90) = 1 - bump_percent_correct(bump_dirs > 90);
-
-        % make psychometric curve
-%         figure();
-        hold on
-        plot(bump_dirs,bump_percent_correct,'.','color',colors{st},'markersize',14)
-%         xlabel('Bump Direction')
-%         ylabel('Percent to 0-deg target')
-%         xlim([0,180])
-%         ylim([0,1])
-%         formatForLee(gcf)
-%         set(gca,'fontsize',14)
-
-        % fit psychometric curve
-        s = fitoptions('Method','NonlinearLeastSquares', 'Startpoint', [.5 -0.5 .1 100],...
-            'MaxFunEvals',10000,'MaxIter',1000,'TolFun',10E-8,'TolX',10E-8,...
-            'Weights',bump_total/sum(bump_total));
-        ft = fittype('a+b*(erf(c*(x-d)))','options',s);
-        
-        [g_mdl.fitObj,g_mdl.gof] = fit(bump_dirs', bump_percent_correct', ft);
-        
-        x_fit = [0:180];
-        y_fit = feval(g_mdl.fitObj,x_fit);
-%         
-        plot(x_fit,y_fit,'color',colors{st},'linewidth',1.5)
-
-    end
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    f.Name = 'Han_20180803_BD_polarDir';
+    saveFiguresLIB(f,input_data.folderpath,f.Name);
     
            
---
+++ 2AFC/plotPsychometricCurve.m
@@ -0,0 +1,46 @@
+function [ f ] = plotPsychometricCurve( psych_data,input_data )
+
+    if(numel(input_data.colors) < numel(psych_data))
+        error('not enough colors')
+    end
+    
+    f=figure();
+    hold on
+    for i = 1:numel(psych_data)
+        % make psychometric curve
+        plot(psych_data(i).bump_dirs,psych_data(i).psych_curve_data,'.','color',input_data.colors{i},'markersize',14)
+        xlabel('Bump Direction')
+        ylabel('Percent to 0-deg target')
+        xlim([0,180])
+        ylim([-0.1,1.1])
+        formatForLee(gcf)
+        ax = gca;
+        ax.FontSize = 14;
+        ax.XTick = 0:45:180;
+        ax.XAxis.MinorTickValues = [0:15:180];
+
+        x_fit = [0:180];
+        y_fit = feval(psych_data(i).psych_fit.fitObj,x_fit);
+%         y_fit_conf = predint(psych_data(i).psych_fit.fitObj,x_fit,0.95,'functional','off');
+        
+        hold on
+        plot(x_fit,y_fit,'color',input_data.colors{i},'linewidth',1.5)
+%         plot(x_fit,y_fit_conf,'color',input_data.colors{i},'linewidth',1.5,'linestyle','--');
+        
+%         if(isfield(psych_data(i),'bootstrap_fit_params')) % deal with the bootstrapped data
+%             % find 2.5 percentile, 97.5 percentile parameters
+%             [sort_param_list] = sort(psych_data(i).bootstrap_fit_params);
+%             conf_idx = ceil(size(psych_data(i).bootstrap_fit_params,1)*[0.025,0.975]);
+%             conf_params = psych_data(i).bootstrap_fit_params(conf_idx,:);
+%             
+%             x_fit = [0:180];
+%             y_fit = conf_params(:,1) + conf_params(:,2).*(erf(conf_params(:,3).*(x_fit-conf_params(:,4))));
+%             
+%             plot(x_fit,y_fit,'color',input_data.colors{i},'linewidth',1.5,'linestyle','--')
+%             
+%         end
+    end
+  
+
+end
+
