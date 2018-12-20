ec4aae773de524ef4c948e18631c7d1587f05895
jtsombeck@gmail.com <jtsombeck@gmail.com>
2018-12-20 10:24:32 -0600

    cleaning files up

--- LIB/getColorFromList.m
+++ LIB/getColorFromList.m
@@ -5,7 +5,7 @@ function [ color_out ] = getColorFromList( list_idx,color_idx )
     switch list_idx
         case 1
             colors_all = [228,26,28;55,126,184;77,175,74;152,78,163;255,127,0;255,255,51;166,86,40;247,129,191;153,153,153]/255;
-            color_out = colors_all(mod(color_idx,numel(colors_all))+1,:);
+            color_out = colors_all(mod(color_idx,size(colors_all,1))+1,:);
 
     end
     
